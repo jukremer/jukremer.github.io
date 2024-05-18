7cb8368f4f3a82e43bb4ae5ab3ed9244f367039e
Jan Kremer <mail@jankremer.eu>
2024-05-18 20:49:17 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,34 @@
 {
   description = "Hugo website";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.go
-            pkgs.hugo
-          ];
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs (systems) f;
+
+      devShell =
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default =
+            with pkgs;
+            mkShell {
+              nativeBuildInputs = with pkgs; [
+                go
+                hugo
+              ];
+            };
         };
-      }
-    );
+    in
+    {
+      devShells = forAllSystems devShell;
+      formatter.x86_64-darwin = nixpkgs.legacyPackages.x86_64-darwin.nixfmt-rfc-style;
+    };
 }
