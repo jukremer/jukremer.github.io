9e8f680c52f851c919c8a7ce8b8b447d0f6b72ad
Jan Kremer <mail@jankremer.eu>
2024-10-17 17:41:01 +0200

    treefmt: fix config, add prettier-plugin-go-template

--- treefmt.nix
+++ treefmt.nix
@@ -1,9 +1,27 @@
+{ pkgs, ... }:
 {
   projectRootFile = "flake.nix";
   programs = {
     nixfmt.enable = true;
     gofmt.enable = true;
     taplo.enable = true;
-    prettier.enable = true;
+    prettier = {
+      enable = true;
+      settings = {
+        plugins = [
+          "${pkgs.prettier-plugin-go-template}/lib/node_modules/prettier-plugin-go-template/lib/index.js"
+        ];
+        overrides = [
+          {
+            files = [ "*.html" ];
+            options.parser = "go-template";
+          }
+        ];
+      };
+    };
   };
+  settings.global.excludes = [
+    "public/**"
+    "static/**"
+  ];
 }
